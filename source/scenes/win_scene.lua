f7c7f1472b6f10fd6cb4b49ca2cd2573b02b3ade
Dennis Aldana <dennis.aldana@gmail.com>
2026-01-31 16:08:42 -0600

    add: happy count

--- source/scenes/win_scene.lua
+++ source/scenes/win_scene.lua
@@ -10,8 +10,9 @@
 
 local gfx = playdate.graphics
 
-function WinScene()
+function WinScene(boidCount)
     local scene = Scene.new("win")
+    local totalBoids = boidCount or 0
 
     function scene:onEnter()
         -- TODO: Play victory sound, save score, etc.
@@ -21,8 +22,8 @@ function WinScene()
         gfx.clear(gfx.kColorWhite)
 
         gfx.drawTextAligned("*YOU WIN!*", 200, 80, kTextAlignment.center)
-        gfx.drawTextAligned("Everyone is happy!", 200, 110, kTextAlignment.center)
-        gfx.drawTextAligned("Press A to play again", 200, 140, kTextAlignment.center)
+        gfx.drawTextAligned("You made " .. totalBoids .. " boids happy!", 200, 110, kTextAlignment.center)
+        gfx.drawTextAligned("Press A to continue", 200, 140, kTextAlignment.center)
 
         if playdate.buttonJustPressed(playdate.kButtonA) then
             GAME_WORLD:queueScene(CreditScene())
