7ecd603bf09cc1efbca67f029e24b6f6bcbb3eb2
oscar_pf <oscar@plasticfernstudios.com>
2026-01-31 15:30:34 -0600

    Added credits scene

--- source/scenes/menu_scene.lua
+++ source/scenes/menu_scene.lua
@@ -22,17 +22,11 @@ function MenuScene()
         gfx.clear(gfx.kColorWhite)
 
         -- Title screen with two scene options
-        gfx.drawTextAligned("*GAME JAM*", 200, 60, kTextAlignment.center)
-        gfx.drawTextAligned("Press A for Game Scene", 200, 120, kTextAlignment.center)
-        gfx.drawTextAligned("Press B for Boid Scene", 200, 140, kTextAlignment.center)
-        gfx.drawTextAligned("Press DOWN for Test Audio Scene", 200, 160, kTextAlignment.center)
+        gfx.drawTextAligned("Masked Boidlings", 200, 60, kTextAlignment.center)
+        gfx.drawTextAligned("Press A to start", 200, 120, kTextAlignment.center)
 
         if playdate.buttonJustPressed(playdate.kButtonA) then
-            GAME_WORLD:queueScene(GameScene())
-        elseif playdate.buttonJustPressed(playdate.kButtonB) then
             GAME_WORLD:queueScene(BoidScene())
-        elseif playdate.buttonJustPressed(playdate.kButtonDown) then
-            GAME_WORLD:queueScene(TestAudioScene())
         end
     end
 
